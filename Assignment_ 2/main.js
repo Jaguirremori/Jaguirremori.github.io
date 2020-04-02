f46895917480b556aeafa871f5442a74e2a4a640
Jaguirremori <J.aguirremori@gmail.com>
2020-04-02 18:37:27 -0400

    Revert "Update main.js"
    This reverts commit dbe58ae1852df8f530c6e09475a69b7a89894ace.

--- Assignment_ 2/main.js
+++ Assignment_ 2/main.js
@@ -5,16 +5,9 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-const images = ["images/pic1.png", "images/pic2.jfif", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
 
-images.forEach(image => {
-    const photo = document.createElement('img');
-    photo.setAttribute('src', image);
-    photo.onclick = ()=>{
-        displayedImage.setAttribute('src', image);
-    }
-    thumbBar.appendChild(photo);
-
-})
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
