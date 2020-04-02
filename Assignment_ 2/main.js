d2c427c64317c78aa71da9f4361db5f7a45f14c3
Jaguirremori <J.aguirremori@gmail.com>
2020-04-02 18:50:41 -0400

    Update

--- Assignment_ 2/main.js
+++ Assignment_ 2/main.js
@@ -5,9 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+const images = ["images/pic1.png", "images/pic2.jfif", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+images.forEach(image => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image);
+    newImage.onclick = () =>{
+        displayedImage.setAttribute('src', image);
+    }
+    thumbBar.appendChild(newImage);
+})
+
 
 /* Wiring up the Darken/Lighten button */
