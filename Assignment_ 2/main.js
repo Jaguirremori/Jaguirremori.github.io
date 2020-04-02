5a8ad50a64219d10c94530ba775fbf140aace9ae
Jaguirremori <J.aguirremori@gmail.com>
2020-04-02 18:41:11 -0400

    Update main.js

--- Assignment_ 2/main.js
+++ Assignment_ 2/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-const images = ["images/pic1.png", "images/pic2.jfif", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
+const photo = ["images/pic1.png", "images/pic2.jfif", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
 
 images.forEach(image => {
     const photo = document.createElement('img');
